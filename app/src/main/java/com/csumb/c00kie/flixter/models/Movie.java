e021c7750a5ea541fedbb7f6cb945ca5b488bac9
cookiewho <dinkwho@gmail.com>
2020-09-19 18:18:29 -0700

    Starting Flixter App with the ability to get info from the The Movie DB API

---
+++ app/src/main/java/com/csumb/c00kie/flixter/models/Movie.java
@@ -0,0 +1,41 @@
+package com.csumb.c00kie.flixter.models;
+
+import com.facebook.stetho.inspector.jsonrpc.JsonRpcException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String poseterPath;
+    String title;
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        poseterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+    public static List<Movie> fromJsonArray(JSONArray moviejsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for(int x = 0; x < moviejsonArray.length(); x++){
+            movies.add(new Movie(moviejsonArray.getJSONObject(x)));
+        }
+        return movies;
+    }
+
+    public String getPoseterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", poseterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
