208f32ebbf5dbb2bd3ba2ab19c8cc9ab331a17b3
cookiewho <dinkwho@gmail.com>
2020-09-24 17:41:53 -0700

    Added detail acvtivity, and made it pass data via parcel using the Parceler library

--- app/src/main/java/com/csumb/c00kie/flixter/models/Movie.java
+++ app/src/main/java/com/csumb/c00kie/flixter/models/Movie.java
@@ -5,22 +5,30 @@ import com.facebook.stetho.inspector.jsonrpc.JsonRpcException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
     String backdropPath;
     String poseterPath;
     String title;
     String overview;
+    double rating;
+
+    // empty constructor needed by the Parceler library
+    public Movie(){}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         poseterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
     }
+
     public static List<Movie> fromJsonArray(JSONArray moviejsonArray) throws JSONException {
         List<Movie> movies = new ArrayList<>();
         for(int x = 0; x < moviejsonArray.length(); x++){
@@ -44,4 +52,9 @@ public class Movie {
     public String getOverview() {
         return overview;
     }
+
+    public double getRating() {
+        return rating;
+    }
+
 }
