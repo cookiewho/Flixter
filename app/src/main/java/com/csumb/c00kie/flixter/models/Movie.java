ea4be52690fff6d512c2b0bc0e817e0604f7f9b8
cookiewho <dinkwho@gmail.com>
2020-09-26 20:35:08 -0700

    plenty of errors, but implemented youtube video API, and have it set up to open trailer to movie when in detailed activity

--- app/src/main/java/com/csumb/c00kie/flixter/models/Movie.java
+++ app/src/main/java/com/csumb/c00kie/flixter/models/Movie.java
@@ -1,7 +1,5 @@
 package com.csumb.c00kie.flixter.models;
 
-import com.facebook.stetho.inspector.jsonrpc.JsonRpcException;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,6 +10,7 @@ import java.util.List;
 
 @Parcel
 public class Movie {
+    int movieId;
     String backdropPath;
     String poseterPath;
     String title;
@@ -21,12 +20,17 @@ public class Movie {
     // empty constructor needed by the Parceler library
     public Movie(){}
 
+    public int getMovieId() {
+        return movieId;
+    }
+
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         poseterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray moviejsonArray) throws JSONException {
